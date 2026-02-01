d46bad7ed29b49ef0a67bbab75dcf7aff624ca24
siddiqa-malik <siddiqamalik888@gmail.com>
2026-02-01 23:30:56 +0500

    update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,13 +1,14 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: import.meta.env.VITE_API_URL  // Vercel backend URL
 });
 
+// Optional: attach token
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
   if (token) {
-    config.headers.Authorization = token;
+    config.headers.Authorization = `Bearer ${token}`; // better with Bearer
   }
   return config;
 });
