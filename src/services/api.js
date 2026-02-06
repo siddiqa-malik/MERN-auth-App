eb39499c6a74a0020806c05b32da9ce7d73d7734
siddiqa-malik <siddiqamalik888@gmail.com>
2026-02-06 23:07:43 +0500

    add updated version

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
+const resolvedBaseURL = import.meta.env.VITE_API_URL || "https://mern-auth-backend-green.vercel.app/api";
+
+
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL
+  baseURL: resolvedBaseURL
 });
 
 api.interceptors.request.use((config) => {
